7c9f2180617ccb7b5528c1893d1326c440584833
T3n4-duck <angelschafer2002@gmail.com>
2021-11-13 23:44:42 -0300

    first commit

---
+++ Js/buy-tickets.js
@@ -0,0 +1,29 @@
+const cantidadEntradas = document.getElementById('Cantidad');
+const botoncalcular = document.getElementById('botonRe');
+const categoria = document.getElementById('inputState');
+const totalCompra = document.getElementById('totalFinal');
+
+function calcularEntradas(){
+    console.log(categoria.value);
+    console.log(cantidadEntradas.value);
+    cantidadEntradas.value;
+
+    if(categoria.value == 1){                                            //Estudiantes
+        const total = ( 200 * 0.2 ) * cantidadEntradas.value;
+        totalCompra.innerHTML = total;
+    }
+    else if(categoria.value == 2){                                       //Trainer
+        const total = ( 200 * 0.5) * cantidadEntradas.value;
+        totalCompra.innerHTML = total;
+    }
+    else if(categoria.value == 3){                                       //Junior
+        const total = ( 200 * 0.85) * cantidadEntradas.value;
+        totalCompra.innerHTML = total;
+    }
+    return false
+}
+
+function reset(){
+    document.getElementsById("myForm").reset();
+    return false
+}
